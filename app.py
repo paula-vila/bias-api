5b957d3eba61797e6204e951280fc42db161df8a
paula-vila <pv2393@columbia.edu>
2026-02-21 00:34:39 -0500

    app.py

--- app.py
+++ app.py
@@ -4,11 +4,19 @@ import os
 
 app = Flask(__name__)
 
-model = joblib.load('bias_model.pkl')
-vectorizer = joblib.load('vectorizer.pkl')
+@app.after_request
+def add_cors(response):
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    response.headers['Access-Control-Allow-Headers'] = 'Content-Type'
+    response.headers['Access-Control-Allow-Methods'] = 'POST, OPTIONS'
+    return response
 
-@app.route('/analyze', methods=['POST'])
+@app.route('/analyze', methods=['POST', 'OPTIONS'])
 def analyze():
+    if request.method == 'OPTIONS':
+        return jsonify({}), 200
+    model = joblib.load('bias_model.pkl')
+    vectorizer = joblib.load('vectorizer.pkl')
     data = request.get_json()
     text = data.get('text', '')
     if not text:
